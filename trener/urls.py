2a1f23489dadd49bd06530662f3418d49a74297d
321nomyzS <balinskiszymon@gmail.com>
2024-04-18 22:37:16 +0200

    Create base template

---
+++ trener/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+import trener.views as views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('exercise/add', views.add_exercise, name='add_exercise'),
+    path('exercise', views.show_exercise, name='show_exercise'),
+    path('training/add', views.add_training, name='add_training'),
+    path('training', views.show_training, name='show_training')
+]
