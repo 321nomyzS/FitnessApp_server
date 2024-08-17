51bd1eb3ccebd76a1274d698db770983e17a5eba
321nomyzS <84841950+321nomyzS@users.noreply.github.com>
2024-08-17 14:59:52 +0200

    Update settings.py

--- FitnessApp_server/settings.py
+++ FitnessApp_server/settings.py
@@ -149,10 +149,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "trener_style/static",
-]
-STATIC_ROOT = BASE_DIR / "trener_style"
+STATIC_ROOT = os.path.join(BASE_DIR, "trener_style/static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
